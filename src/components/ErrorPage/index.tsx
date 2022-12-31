d1ef19606e2199a606abc1a39233476e99e59d62
xiewen <1123654054@qq.com>
2022-12-31 17:54:24 +0800

    modify

---
+++ src/components/ErrorPage/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+export const ErrorPage: React.FC =() =>{
+return <div>ErrorPage </div>;
+}; 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from 'react-dom/client';
+import { ErrorPage } from './components/ErrorPage';
+import App from './App';
+import { Outlet,createBrowserRouter,RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div><Outlet /></div>,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        index:true,
+        element: <div>主页</div>,
+      },
+      {
+        path:'/1',
+        element: <div>1</div>,
+      },
+      {
+        path:'/2',
+        element: <div>2</div>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
