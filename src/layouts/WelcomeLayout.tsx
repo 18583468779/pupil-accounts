f3018e71a9134a4d1caae9d6f227ca905465bdfc
xiewen <xw18583468779@gmail.com>
2023-01-05 13:43:04 +0800

    update

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -4,7 +4,7 @@ import { useRef } from 'react'
 import { Link, useLocation, useOutlet } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
 
-const linkMap = {
+const linkMap: Record<string, string> = {
   '/welcome/1': '/welcome/2',
   '/welcome/2': '/welcome/3',
   '/welcome/3': '/welcome/4',
@@ -16,29 +16,29 @@ export const WelcomeLayout: React.FC = () => {
   const location = useLocation()
   const outlet = useOutlet()
   map.current[location.pathname] = outlet
-  const [extraStyle,setExtraStyle] = useState({position:'relative'})
+  const [extraStyle, setExtraStyle] = useState<{ position: 'relative' | 'absolute' } > ({ position: 'relative' })
   const transitions = useTransition(location.pathname, {
     from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
     config: { duration: 300 },
     // 动画定位的问题
-    onStart:()=>{
-      setExtraStyle({position:'absolute'})
+    onStart: () => {
+      setExtraStyle({ position: 'absolute' })
     },
-    onRest:()=>{
-      setExtraStyle({position:'relative'})
+    onRest: () => {
+      setExtraStyle({ position: 'relative' })
     }
   })
   return (
    <div className='bg-#5f34bf h-screen flex flex-col justify-center items-strech pb-16px'>
     <header shrink-0 text-center pt-44px>
-      <img src={logo} w-64px/>
+      <img src={logo} w-64px h-69px/>
       <h1 text="#D4D4EE">小学生记账</h1>
     </header>
-    <main shrink-1 grow-1  relative>
+    <main shrink-1 grow-1 relative>
         {transitions((style, pathname) =>
-        <animated.div key={pathname} style={{...style,...extraStyle}} w="100%" h="100%" p-16px flex>
+        <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%" p-16px flex>
          <div grow-1 bg-white rounded-8px flex justify-center items-center>
           {map.current[pathname]}
          </div>
