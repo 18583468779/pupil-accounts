5c5fab51699e22ed0d21aa90e1bdc87832aac8af
xiewen <1123654054@qq.com>
2023-02-04 14:41:26 +0800

    封装表单组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+type Props={
+    label:string
+    placeholder?:string
+    type?:'text' | 'emoji'
+    value?:string
+    onChange?:(value:string)=>void
+    error?:string
+}
+export const Input: React.FC<Props> =(props) =>{
+    const {label,placeholder,type,value,onChange,error} = props
+return (<div flex flex-col gap-y-8px>
+            <span text='18px'>{label}</span>
+            <input j-input-text type={type} placeholder={placeholder}
+            value={value} onChange={ e => onChange?.(e.target.value)} />
+            <span text-red text-12px>{ error || '\u00A0'}</span>
+        </div>)
+} 
--- src/pages/TagsNewPage.tsx
+++ src/pages/TagsNewPage.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import { Gradient } from "../components/Gradient";
 import { Icon } from "../components/Icon";
+import { Input } from "../components/Input";
 import { TopNav } from "../components/TopNav";
 import { emojis } from "../lib/emojis";
 
@@ -13,29 +14,29 @@ export const TagsNewPage: React.FC = () => {
       <Gradient className="grow-0 shrink-0">
         <TopNav title="记一笔" icon={<Icon name="back" />} />
       </Gradient>
-      <form onSubmit={onSubmit}>
-        <div>
-            <span>标签名</span>
-            <input type="text" j-input-text/>
-            <span text-red>标签名太长</span>
-        </div>
-        <div>
-            <span>符号 🤤</span>
-            <div>
-                <div flex flex-nowrap>
+      <form onSubmit={onSubmit} px-16px py-32px flex flex-col gap-y-8px>
+      <Input label="标签名" error="标签名太长"/>
+        <div flex flex-col gap-y-8px>
+            <span text-18px>符号 <span text-24px>🤤</span> </span>
+            <div b-1 b="#5c33be" rounded-8px>
+                <div flex flex-nowrap p-8px gap-x-16px overflow-auto>
                 {
                     emojis.map(emoji =>
-                     <span key={emoji.name} onClick={()=>setEmojiKind(emoji.name)}>{emoji.name}</span>    
+                     <span whitespace-nowrap key={emoji.name} 
+                     onClick={()=>setEmojiKind(emoji.name)}
+                     className={emoji.name === emojiKind? 'red' : ''}
+                     color="#999"
+                     >{emoji.name}</span>    
                     )
                 }
                 </div>
-                <div>
+                <div text-24px p-8px h-400px overflow-auto >
                     {
-                        emojis.map(emoji => <div style={{display:emojiKind===emoji.name ? '' :'none'}}>{emoji.chars}</div>)
+                        emojis.map(emoji => <div flex flex-wrap style={{display:emojiKind===emoji.name ? '' :'none'}}>{emoji.chars.map(char => <span text="center" w-34px>{char}</span>)}</div>)
                     }
                 </div>
             </div>
-            <p>记账时长按标签，进行编辑</p>
+            <p text-center py-24px>记账时长按标签，进行编辑</p>
             <div>
                 <button j-btn>确定</button>
             </div>
