c140035759a7ea51a7b1b2a00be85443c9985671
xiewen <1123654054@qq.com>
2023-02-16 21:55:51 +0800

    modify

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,78 +1,115 @@
-import axios, { AxiosError } from 'axios'
-import type { FormEventHandler } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Gradient } from '../components/Gradient'
-import { Icon } from '../components/Icon'
-import { Input } from '../components/Input'
-import { TopNav } from '../components/TopNav'
-import { ajax } from '../lib/ajax'
-import { FormError, hasError, validate } from '../lib/validate'
-import { useSignInStore } from '../stores/useSignInStore'
+import axios, { AxiosError } from "axios";
+import type { FormEventHandler } from "react";
+import { useNavigate } from "react-router-dom";
+import { Gradient } from "../components/Gradient";
+import { Icon } from "../components/Icon";
+import { Input } from "../components/Input";
+import { TopNav } from "../components/TopNav";
+import { ajax } from "../lib/ajax";
+import { FormError, hasError, validate } from "../lib/validate";
+import { useSignInStore } from "../stores/useSignInStore";
 
 export const SignInPage: React.FC = () => {
-  const { data, error, setData, setError } = useSignInStore()
-  const nav = useNavigate()
-  const onSubmitError = (err: AxiosError<{ errors: FormError<typeof data> }>) => {
-    setError(err.response?.data?.errors ?? {})
-    throw error
-  }
+  const { data, error, setData, setError } = useSignInStore();
+  const nav = useNavigate();
+  const onSubmitError = (
+    err: AxiosError<{ errors: FormError<typeof data> }>
+  ) => {
+    setError(err.response?.data?.errors ?? {});
+    throw error;
+  };
   const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newError = validate(data, [
-      { key: 'email', type: 'required', message: '请输入邮箱地址' },
-      { key: 'email', type: 'pattern', regex: /^.+@.+$/, message: '邮箱地址格式不正确' },
-      { key: 'code', type: 'required', message: '请输入验证码' },
-      { key: 'code', type: 'length', min: 6, max: 6, message: '验证码必须是6个字符' },
-    ])
-    setError(newError)
+      { key: "email", type: "required", message: "请输入邮箱地址" },
+      {
+        key: "email",
+        type: "pattern",
+        regex: /^.+@.+$/,
+        message: "邮箱地址格式不正确",
+      },
+      { key: "code", type: "required", message: "请输入验证码" },
+      {
+        key: "code",
+        type: "length",
+        min: 6,
+        max: 6,
+        message: "验证码必须是6个字符",
+      },
+    ]);
+    setError(newError);
     if (!hasError(newError)) {
-      const response = await ajax.post<{ jwt: string }>('http://121.196.236.94:8080/api/v1/session', data)
-        .catch(onSubmitError)
-      const jwt = response.data.jwt
-      console.log('jwt', jwt)
-      localStorage.setItem('jwt', jwt)
-      nav('/home')
+      const response = await ajax
+        .post<{ jwt: string }>(
+          "http://121.196.236.94:8080/api/v1/session",
+          data
+        )
+        .catch(onSubmitError);
+      const jwt = response.data.jwt;
+      console.log("jwt", jwt);
+      localStorage.setItem("jwt", jwt);
+      nav("/home");
     }
-  }
+  };
   const sendSmsCode = async () => {
     const newError = validate({ email: data.email }, [
-      { key: 'email', type: 'pattern', regex: /^.+@.+$/, message: '邮箱地址格式不正确' }
-    ])
-    setError(newError)
+      {
+        key: "email",
+        type: "pattern",
+        regex: /^.+@.+$/,
+        message: "邮箱地址格式不正确",
+      },
+    ]);
+    setError(newError);
     if (hasError(newError)) {
-      console.log('有错')
+      console.log("有错");
     } else {
-      console.log('没错')
+      console.log("没错");
       // 请求
-      const response = await axios.post('http://121.196.236.94:8080/api/v1/validation_codes', {
-        email: data.email
-      })
-      return response
+      const response = await axios.post(
+        "http://121.196.236.94:8080/api/v1/validation_codes",
+        {
+          email: data.email,
+        }
+      );
+      return response;
     }
-  }
+  };
   return (
     <div>
       <Gradient>
         <TopNav title="登录" icon={<Icon name="back" />} />
       </Gradient>
       <div text-center pt-40px pb-16px>
-        <Icon name="logo" className='w-64px h-68px' />
-        <h1 text-32px text="#7878FF" font-bold>山竹记账</h1>
+        <Icon name="logo" className="w-64px h-68px" />
+        <h1 text-32px text="#7878FF" font-bold>
+          山竹记账
+        </h1>
       </div>
       <form j-form onSubmit={onSubmit}>
-        <Input label='邮箱地址' type="text" placeholder='请输入邮箱，然后点击发送验证码'
-          value={data.email} onChange={value => setData({ email: value })}
+        <Input
+          label="邮箱地址"
+          type="text"
+          placeholder="请输入邮箱，然后点击发送验证码"
+          value={data.email}
+          onChange={(value) => setData({ email: value })}
           error={error.email?.[0]}
         />
-        <Input type='sms_code' label='验证码' placeholder='六位数字' 
-          value={data.code} 
-          onChange={value => setData({ code: value })}
+
+        <Input
+          type="sms_code"
+          label="验证码"
+          placeholder="六位数字"
+          value={data.code}
+          onChange={(value) => setData({ code: value })}
           request={sendSmsCode}
         />
         <div mt-100px>
-          <button j-btn type="submit" >登录</button>
+          <button j-btn type="submit">
+            登录
+          </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
