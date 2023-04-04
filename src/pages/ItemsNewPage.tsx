69c61b8d20e422d416e6dc75148430f0baa1e461
xiewen <1123654054@qq.com>
2023-04-04 15:46:51 +0800

    校验并提交表单数据（新建item页面）

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -8,6 +8,8 @@ import { ItemAmount } from './ItemsNewPage/ItemAmount'
 import { Tags } from './ItemsNewPage/Tags'
 import { useCreateItemStore } from '../stores/useCreateItemStore'
 import { ItemDate } from './ItemsNewPage/ItemDate'
+import { hasError, validate } from '../lib/validate'
+import { useAjax } from '../lib/ajax'
 
 export const ItemsNewPage: React.FC = () => {
   const {data,error,setData,setError} = useCreateItemStore();
@@ -17,9 +19,24 @@ export const ItemsNewPage: React.FC = () => {
       { key: 'income', text: '收入', 
         element: <Tags kind="income"  value={data.tag_ids} onchange={(ids)=>setData({tag_ids:ids})} /> }
   ]
-  const onSubmit:FormEventHandler<HTMLFormElement> = (e) =>{
+  const {post} = useAjax({showLoading:true,handleError:true})
+  const onSubmit:FormEventHandler<HTMLFormElement> =async (e) =>{
     e.preventDefault()
-    console.log('提交')
+   const error = validate(data,[
+      {key:'kind',type:'required',message:"请选择类型：收入还是支出"},
+      {key:'tag_ids',type:'required',message:"请选择一个标签"},
+      {key:'happen_at',type:'required',message:"请选择一个时间"},
+      {key:'amount',type:'required',message:"请输入金额"},
+      {key:'amount',type:'notEqual',value:0,message:"金额不能为0"},
+    ]);
+    setError(error);
+    if(hasError(error)){
+      const message = Object.values(error).flat().join('\n');
+      window.alert(message)
+    }else{
+     const response = await post<Resource<Item>>('/api/v1/items',data);
+     console.log(response.data.resource)
+    }
   }
   return (
     <form className={s.wrapper} h-screen flex flex-col onSubmit={onSubmit}>
@@ -29,7 +46,6 @@ export const ItemsNewPage: React.FC = () => {
       <Tabs tabItems={tabItems} className="text-center grow-1 shrink-1 overflow-hidden" classPrefix='itemsNewPage'
         value={data.kind!}
         onChange={(tabItem) => { setData({kind:tabItem}) }} />
-        <div>{JSON.stringify(data)}</div>
       <ItemAmount className="grow-0 shrink-0" value={data.amount} onChange={(amount)=>{setData({amount})}}
         ItemDate={<ItemDate value={data.happen_at} onChange={(d)=>{setData({happen_at:d})}}/>} 
       />
