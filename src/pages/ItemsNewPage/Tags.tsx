0170e1c39af43ebea330a72c666eedd147e5bf9f
xiewen <1123654054@qq.com>
2023-04-06 09:48:18 +0800

    长按sign进入编辑页

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,23 +1,22 @@
-import { Link } from 'react-router-dom'
-import { Icon } from '../../components/Icon'
-import { useAjax } from '../../lib/ajax';
+import type { TouchEvent } from 'react'
+import { useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
 import useSWRInfinite from 'swr/infinite'
-import styled from 'styled-components';
+import { Icon } from '../../components/Icon'
+import { useAjax } from '../../lib/ajax'
+
 type Props = {
   kind: Item['kind']
-  value?:Item['tag_ids']
-  onchange?:(ids:Item['tag_ids'])=>void
+  value?: Item['tag_ids']
+  onChange?: (ids: Item['tag_ids']) => void
 }
-
 const Div = styled.div`
   padding: 16px;
   text-align: center;
 `
-
-
-
 export const Tags: React.FC<Props> = (props) => {
-  const { kind } = props;
+  const { kind } = props
   const getKey = (pageIndex: number, prev: Resources<Item>) => {
     if (prev) {
       const sendCount = (prev.pager.page - 1) * prev.pager.per_page + prev.resources.length
@@ -27,22 +26,47 @@ export const Tags: React.FC<Props> = (props) => {
     return `/api/v1/tags?page=${pageIndex + 1}&kind=${kind}`
   }
 
-  const {get} = useAjax({showLoading:true,handleError:true}) 
+  const { get } = useAjax({ showLoading: true, handleError: true })
   const { data, error, size, setSize } = useSWRInfinite(
     getKey,
     async path => (await get<Resources<Tag>>(path)).data,
     { revalidateFirstPage: false }
   )
-  const onLoadMore = () => {
-    setSize(size + 1)
-  }
   const isLoadingInitialData = !data && !error
   const isLoadingMore = data?.[size - 1] === undefined && !error
   const isLoading = isLoadingInitialData || isLoadingMore
-
-  if(!data){
+  const onLoadMore = () => {
+    setSize(size + 1)
+  }
+  const touchTimer = useRef<number>()
+  const touchPosition = useRef<{ x?: number; y?: number }>({ x: undefined, y: undefined })
+  const nav = useNavigate()
+  const onTouchStart = (e: TouchEvent<HTMLLIElement>, id: Tag['id']) => {
+    touchTimer.current = window.setTimeout(() => {
+      nav(`/tags/${id}`)
+    }, 500)
+    const { clientX: x, clientY: y } = e.touches[0]
+    touchPosition.current = { x, y }
+  }
+  const onTouchMove = (e: TouchEvent<HTMLLIElement>, id: Tag['id']) => {
+    const { clientX: newX, clientY: newY } = e.touches[0]
+    const { x, y } = touchPosition.current
+    if (x === undefined || y === undefined) { return }
+    const distance = Math.sqrt((newX - x) ** 2 + (newY - y) ** 2)
+    if (distance > 10) {
+      window.clearTimeout(touchTimer.current)
+      touchTimer.current = undefined
+    }
+  }
+  const onTouchEnd = (e: TouchEvent<HTMLLIElement>, id: Tag['id']) => {
+    if (touchTimer.current) {
+      window.clearTimeout(touchTimer.current)
+      touchTimer.current = undefined
+    }
+  }
+  if (!data) {
     return <div>空</div>
-  }else{
+  } else {
     const last = data[data.length - 1]
     const { page, per_page, count } = last.pager
     const hasMore = (page - 1) * per_page + last.resources.length < count
@@ -52,39 +76,39 @@ export const Tags: React.FC<Props> = (props) => {
           gap-y-16px py-16px px-8px>
           <li>
             <Link to={`/tags/new?kind=${kind}`}>
-               <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
+              <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
                 flex justify-center items-center text-24px text="#8F4CD7"
-                ><Icon name="add" /></span>
+              ><Icon name="add" /></span>
             </Link>
           </li>
           {
-            data.map(({resources},index)=>{
+            data.map(({ resources }, index) => {
               return resources.map((tag, index) =>
-              <li key={index} w-48px flex justify-center items-center flex-col gap-y-8px 
-               onClick={()=>{props.onchange?.([tag.id])}}
-              >
-                {
-                  props.value?.includes(tag.id) 
-                  ? <span block w-48px h-48px rounded="24px" bg="#8F4CD7"
+                <li key={index} w-48px flex justify-center items-center flex-col gap-y-8px
+                  onClick={() => { props.onChange?.([tag.id]) }}
+                  onTouchStart={(e) => onTouchStart(e, tag.id)}
+                  onTouchMove={(e) => onTouchMove(e, tag.id)}
+                  onTouchEnd={(e) => onTouchEnd(e, tag.id)}
+                >
+                  {props.value?.includes(tag.id)
+                    ? <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
                       flex justify-center items-center text-24px b-1 b="#8F4CD7">{tag.sign}</span>
-                  : <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
-                      flex justify-center items-center text-24px b-1 b="transparent">{tag.sign}</span>
-                }
-                    <span text-12px text="#666">{tag.name}</span>
-              </li>
-            )
+                    : <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
+                      flex justify-center items-center text-24px b-1 b-transparent>{tag.sign}</span>
+                  }
+                  <span text-12px text="#666">{tag.name}</span>
+                </li>
+              )
             })
           }
         </ol>
         {error && <Div>数据加载失败，请刷新页面</Div>}
         {!hasMore
-          ? page === 1 && count === 0 ? <Div>点击加号，创建新标签</Div> : <Div>没有更多数据</Div>
+          ? page === 1 && last.resources.length === 0 ? <Div>点击加号，创建新标签</Div> : <Div>没有更多数据了</Div>
           : isLoading
             ? <Div>数据加载中...</Div>
             : <Div><button j-btn onClick={onLoadMore}>加载更多</button></Div>}
       </div>
     )
   }
-
-
 }
