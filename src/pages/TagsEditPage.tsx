ac09a050a5d287149e7d82ee26ed36954fb6661d
xiewen <1123654054@qq.com>
2023-04-06 14:36:40 +0800

    删除tag功能完成

--- src/pages/TagsEditPage.tsx
+++ src/pages/TagsEditPage.tsx
@@ -3,7 +3,26 @@ import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
 import { TagForm } from './TagsNewPage/TagForm'
+import { useAjax } from '../lib/ajax'
+import { useParams } from 'react-router-dom'
 export const TagsEditPage: React.FC = () => {
+
+  const confirmable = (fn:()=>void) =>()=>{
+    const result = window.confirm('您确认要删除吗？');
+    if(result){
+      fn()
+    }
+  } 
+
+  const {destroy} = useAjax({showLoading:true,handleError:true});
+  const params = useParams();
+  const onDelete = confirmable(async ()=>{
+    if(!params.id){ 
+      throw new Error('id不能为空')
+    }
+    await destroy(`/api/v1/tags/${params.id}`);
+    window.alert('删除成功')
+  })
   return (
 <div>
     <Gradient className="grow-0 shrink-0">
@@ -11,7 +30,7 @@ export const TagsEditPage: React.FC = () => {
     </Gradient>
      <TagForm type="edit"/>
      <div px-16px p-b-32px>
-        <button j-btn bg-red>删除</button>
+        <button j-btn bg-red onClick={onDelete}>删除</button>
      </div>
   </div>)
 }
